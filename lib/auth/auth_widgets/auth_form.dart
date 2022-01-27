099fe82bb3d8c8151e8dfada378bd865a07dea88
Basith P <basithpofficial@gmail.com>
2022-01-28 02:31:29 +0530

    Save form values

--- lib/auth/auth_widgets/auth_form.dart
+++ lib/auth/auth_widgets/auth_form.dart
@@ -15,7 +15,13 @@ class _AuthFormState extends State<AuthForm> {
   String _passwd = '';
 
   void _submitForm() {
-    _formKey.currentState!.validate();
+    if (_formKey.currentState!.validate()) {
+      FocusScope.of(context).unfocus();
+      _formKey.currentState!.save();
+      print('Username' + _username);
+      print('_email' + _email);
+      print('padd' + _passwd);
+    }
   }
 
   @override
@@ -41,33 +47,33 @@ class _AuthFormState extends State<AuthForm> {
                   onSaved: (value) => _email = value!,
                 ),
                 TextFormField(
-                    decoration: const InputDecoration(labelText: 'Username'),
-                    validator: (value) {
-                      if (value!.isEmpty) {
-                        return "Please provide a username";
-                      }
-                      if (value.length < 3) {
-                        return "username should be at least 3 Characters long";
-                      }
-                    },
-                    onSaved: (value) => _username = value!),
+                  decoration: const InputDecoration(labelText: 'Username'),
+                  validator: (value) {
+                    if (value!.isEmpty) {
+                      return "Please provide a username";
+                    }
+                    if (value.length < 3) {
+                      return "username should be at least 3 Characters long";
+                    }
+                  },
+                  onSaved: (value) => _username = value!,
+                ),
                 TextFormField(
-                    decoration: const InputDecoration(labelText: 'Password'),
-                    obscureText: true,
-                    validator: (value) {
-                      if (value!.isEmpty) {
-                        return "Please provide a password";
-                      }
-                      if (value.length < 6) {
-                        return "Password should be at least 6 Characters long";
-                      }
-                    },
-                    onSaved: (value) => _passwd = value!),
+                  decoration: const InputDecoration(labelText: 'Password'),
+                  obscureText: true,
+                  validator: (value) {
+                    if (value!.isEmpty) {
+                      return "Please provide a password";
+                    }
+                    if (value.length < 6) {
+                      return "Password should be at least 6 Characters long";
+                    }
+                  },
+                  onSaved: (value) => _passwd = value!,
+                ),
                 const SizedBox(height: 30),
                 ElevatedButton(
-                  onPressed: () {
-                    _submitForm();
-                  },
+                  onPressed: _submitForm,
                   child: Text('Login'),
                 ),
                 TextButton(
